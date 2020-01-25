58ad9c70157bd60c3adcd6cab07a33bac51a0447
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-01-26 00:47:15 +0100

    Implement parsing of multiple corrections

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,37 @@
 use difference::{Difference, Changeset};
 
+
 use nom::{
     IResult,
     sequence::{delimited, preceded, tuple},
-    character::complete::char,
-    bytes::complete::{is_not, tag},
-    combinator::opt,
-    multi::{many0, separated_list},
+    character::complete::{char, newline},
+    bytes::complete::{is_not, tag, take_while, take},
+    combinator::{opt, not},
+    multi::{many0, many1,count, separated_list},
     branch::alt,
+    many_till,
 };
 
 #[derive(Debug)]
 struct Correction<'a>{
     before: &'a str,
-    after: &'a Vec<&'a str>,
+    after: Vec<&'a str>,
     explanation: Option<&'a str>,
 }
 
 fn main() {
     let cor = Correction {
         before : "a new way",
-        after : &vec!["another way", "one way"],
+        after : vec!["another way", "one way"],
         explanation : Some("[this is how it works]"),
     };
     beautify_correction_for_msg(cor);
-    let text = "-  this is the old way\n+ this is the new way(OR)this is a newer way\n[explanation]\n\n";
-    let (i, a) = source_line(text).unwrap();
-    let (i, b) = corrections_line(i).unwrap();
-    let b = b.split("(OR)").collect::<Vec<&str>>();
-    println!("{}", i);
-    println!("{}", a);
-    println!("{:?}", b);
-    let (i, c) = explanation_line(i).unwrap();
-    let cor = Correction {
-        before: a,
-        after: &b,
-        explanation: Some(c),
-    };
-    beautify_correction_for_msg(cor);
+    let text = "-  this is the old way\n+ this is the new way(OR)this is a newer way\n\n\n-bad expression\n+good exprsesion\n[words of explanation]\n";
+    let (i, cors) = parse_all_corrections(text).unwrap();
+    for cor in cors {
+        beautify_correction_for_msg(cor);
+    }
+    println!(">{}<", i);
 }
 
 fn beautify_correction_for_msg(correc: Correction){
@@ -50,7 +44,6 @@ fn beautify_correction_for_msg(correc: Correction){
         Some(expl) => println!("_[{}]_", expl),
             None => println!(),
         }
-    println!();
 }
 
 fn beautify_for_msg(chngset: Changeset){
@@ -77,18 +70,36 @@ fn strike(text: &str) {
 
 //NOM
 fn source_line(i: &str) -> IResult<&str, &str>{
-    let minus = tuple((char('-'), many0(char(' ')))); // "-[ ]*"
+    let minus = tuple(( many0(char('\n')),char('-'), many0(char(' ')))); // "-[ ]*"
     let nl = char('\n');
     delimited(minus, is_not("\n"), nl)(i)
 }
 
-
-fn corrections_line(i: &str) -> IResult<&str, &str>{
+fn corrections_line(i: &str) -> IResult<&str, Vec<&str>>{
     let plus = tuple((char('+'), many0(char(' ')))); // "+[ ]*"
     let nl = char('\n');
-    delimited(plus, is_not("\n"), nl)(i)
+    let (i, res) = delimited(plus, is_not("\n"), nl)(i)?;
+    Ok((i, res.split("(OR)").collect::<Vec<&str>>()))
 }
 
-fn explanation_line(i:&str) -> IResult<&str, &str>{
-    delimited(char('['), is_not("]"), char(']'))(i) //CAUTION: last coorection or explanation may not end with a new line
+fn explanation_line(i:&str) -> IResult<&str, Option<&str>>{
+    let expl_end = tuple((char(']'), many0(char('\n')))); // "-[ ]*"
+    opt(delimited(char('['), is_not("]"), expl_end))(i) //CAUTION: last coorection or explanation may not end with a new line
+}
+
+fn parse_a_correction(i: &str) -> IResult<&str, Correction>{
+    let (i, src) = source_line(i).unwrap();
+    let (i, correcs) = corrections_line(i).unwrap();
+    let (i, expl) = explanation_line(i).unwrap();
+    let correction = Correction{
+        before: src,
+        after: correcs,
+        explanation: expl,
+    };
+    Ok((i, correction))
+}
+
+fn parse_all_corrections(i: &str) -> IResult<&str, Vec<Correction>>{
+    let (i, c) = separated_list(char('\n'), parse_a_correction)(i).unwrap();
+    Ok((i, c))
 }
