3310b75a96eca747dd25c863d693c895cdd37d41
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-01-29 20:06:47 +0100

    Refactor: extract parsing module

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,10 @@ use std::fs;
 use std::env;
 use difference::{Difference, Changeset};
 
-
-use nom::{
-    IResult,
-    sequence::{delimited, preceded, tuple},
-    character::complete::{char, newline},
-    bytes::complete::{is_not, tag, take_while, take},
-    combinator::{opt, not},
-    multi::{many0, many1,count, separated_list},
-    branch::alt,
-    many_till,
-};
+mod parsing;
 
 #[derive(Debug)]
-struct Correction<'a>{
+pub struct Correction<'a>{
     before: &'a str,
     after: Vec<&'a str>,
     explanation: Option<&'a str>,
@@ -57,7 +47,7 @@ fn main() {
     let filename = args.next().expect("Expected filename as first argument");
     let outformat = args.next();
     let contents = fs::read_to_string(&filename).expect("Cannot open file");
-    let (_, cors) = parse_all_corrections(&contents).unwrap();
+    let (_, cors) = parsing::parsing::parse_all_corrections(&contents).unwrap();
     let to_default_message_format =
         if let Some(fmt) = outformat {
             !(fmt == "html" || fmt == "htm")
@@ -137,40 +127,4 @@ fn beautify_for_inline_html(chngset: Changeset){
             Difference::Rem(r) => print!("<del>{}</del>", r),
         }
     }
-}
-
-//NOM
-fn source_line(i: &str) -> IResult<&str, &str>{
-    let minus = tuple(( many0(char('\n')),char('-'), many0(char(' ')))); // "-[ ]*"
-    let nl = char('\n');
-    delimited(minus, is_not("\n"), nl)(i)
-}
-
-fn corrections_line(i: &str) -> IResult<&str, Vec<&str>>{
-    let plus = tuple((char('+'), many0(char(' ')))); // "+[ ]*"
-    let nl = char('\n');
-    let (i, res) = delimited(plus, is_not("\n"), nl)(i)?;
-    Ok((i, res.split("(OR)").collect::<Vec<&str>>()))
-}
-
-fn explanation_line(i:&str) -> IResult<&str, Option<&str>>{
-    let expl_end = tuple((char(']'), many0(char('\n')))); // "-[ ]*"
-    opt(delimited(char('['), is_not("]"), expl_end))(i) //CAUTION: last coorection or explanation may not end with a new line
-}
-
-fn parse_a_correction(i: &str) -> IResult<&str, Correction>{
-    let (i, src) = source_line(i).unwrap();
-    let (i, correcs) = corrections_line(i).unwrap();
-    let (i, expl) = explanation_line(i).unwrap();
-    let correction = Correction{
-        before: src,
-        after: correcs,
-        explanation: expl,
-    };
-    Ok((i, correction))
-}
-
-fn parse_all_corrections(i: &str) -> IResult<&str, Vec<Correction>>{
-    let (i, c) = separated_list(char('\n'), parse_a_correction)(i).unwrap();
-    Ok((i, c))
 }
---
+++ src/parsing.rs
@@ -0,0 +1,48 @@
+//NOM
+pub mod parsing{
+
+    use crate::Correction;
+    use nom::{
+        IResult,
+        sequence::{delimited, tuple},
+        character::complete::char,
+        bytes::complete::is_not,
+        combinator::opt,
+        multi::{many0, separated_list},
+    };
+
+    fn source_line(i: &str) -> IResult<&str, &str>{
+        let minus = tuple(( many0(char('\n')),char('-'), many0(char(' ')))); // "-[ ]*"
+        let nl = char('\n');
+        delimited(minus, is_not("\n"), nl)(i)
+    }
+
+    fn corrections_line(i: &str) -> IResult<&str, Vec<&str>>{
+        let plus = tuple((char('+'), many0(char(' ')))); // "+[ ]*"
+        let nl = char('\n');
+        let (i, res) = delimited(plus, is_not("\n"), nl)(i)?;
+        Ok((i, res.split("(OR)").collect::<Vec<&str>>()))
+    }
+
+    fn explanation_line(i:&str) -> IResult<&str, Option<&str>>{
+        let expl_end = tuple((char(']'), many0(char('\n')))); // "-[ ]*"
+        opt(delimited(char('['), is_not("]"), expl_end))(i) //CAUTION: last coorection or explanation may not end with a new line
+    }
+
+    fn parse_a_correction(i: &str) -> IResult<&str, Correction>{
+        let (i, src) = source_line(i).unwrap();
+        let (i, correcs) = corrections_line(i).unwrap();
+        let (i, expl) = explanation_line(i).unwrap();
+        let correction = Correction{
+            before: src,
+            after: correcs,
+            explanation: expl,
+        };
+        Ok((i, correction))
+    }
+
+    pub fn parse_all_corrections(i: &str) -> IResult<&str, Vec<Correction>>{
+        let (i, c) = separated_list(char('\n'), parse_a_correction)(i).unwrap();
+        Ok((i, c))
+    }
+}
